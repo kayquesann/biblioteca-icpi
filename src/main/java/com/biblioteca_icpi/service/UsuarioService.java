afa48908345fdd004ef07d166c2e273133343b6f
kayquesann <kayque.ferreirasan@gmail.com>
2025-10-25 00:52:49 -0300

    alterando lógica para permitir buscar entidades no banco por ID

--- src/main/java/com/biblioteca_icpi/service/UsuarioService.java
+++ src/main/java/com/biblioteca_icpi/service/UsuarioService.java
@@ -1,7 +1,7 @@
 package com.biblioteca_icpi.service;
 
-import com.biblioteca_icpi.exception.UsuarioJaExistenteException;
-import com.biblioteca_icpi.exception.UsuarioNaoEncontradoException;
+import com.biblioteca_icpi.exception.usuario.UsuarioJaExistenteException;
+import com.biblioteca_icpi.exception.usuario.UsuarioNaoEncontradoException;
 import com.biblioteca_icpi.model.Usuario;
 import com.biblioteca_icpi.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
@@ -17,12 +17,12 @@ public class UsuarioService {
         this.usuarioRepository = usuarioRepository;
     }
 
-    Usuario buscarUsuarioNoBanco (Usuario usuario) {
-        return usuarioRepository.findByEmail(usuario.getEmail()).orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado!"));
+    Usuario buscarUsuarioNoBanco (Long id) {
+        return usuarioRepository.findById(id).orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado!"));
     }
 
     public Usuario criarUsuario(Usuario usuario) {
-        Optional<Usuario> possivelUsuario = usuarioRepository.findByEmail(usuario.getEmail());
+        Optional<Usuario> possivelUsuario = usuarioRepository.findById(usuario.getId());
         if (possivelUsuario.isPresent()) {
             throw new UsuarioJaExistenteException("Usuário já cadastrado!");
         } else {
@@ -30,20 +30,20 @@ public class UsuarioService {
         }
     }
 
-    public Usuario editarUsuario(Usuario usuario) {
-        Usuario usuarioEncontrado = buscarUsuarioNoBanco(usuario);
+    public Usuario editarUsuario(Long id, Usuario usuario) {
+        Usuario usuarioEncontrado = buscarUsuarioNoBanco(id);
         usuarioEncontrado.setNome(usuario.getNome());
         usuarioEncontrado.setSenha(usuario.getSenha());
         return usuarioRepository.save(usuarioEncontrado);
     }
 
     public void excluirUsuario(Long id) {
-        Usuario usuarioEncontrado = usuarioRepository.findById(id).orElseThrow(() -> new UsuarioNaoEncontradoException("Usuario não encontrado!"));
-            usuarioRepository.delete(usuarioEncontrado);
+        Usuario usuarioEncontrado = buscarUsuarioNoBanco(id);
+        usuarioRepository.delete(usuarioEncontrado);
     }
 
     public Usuario lerUsuario(Long id) {
-       return usuarioRepository.findById(id).orElseThrow(() -> new UsuarioNaoEncontradoException("Usuario não encontrado!"));
+       return buscarUsuarioNoBanco(id);
 
     }
 
