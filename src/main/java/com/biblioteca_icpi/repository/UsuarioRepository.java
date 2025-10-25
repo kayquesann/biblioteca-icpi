4470f0e2025032b37a518c43069bf78f024e64f0
kayquesann <kayque.ferreirasan@gmail.com>
2025-10-25 00:51:21 -0300

    exclusão do método findByEmail da classe UsuarioRepository

--- src/main/java/com/biblioteca_icpi/repository/UsuarioRepository.java
+++ src/main/java/com/biblioteca_icpi/repository/UsuarioRepository.java
@@ -4,10 +4,6 @@ import com.biblioteca_icpi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-    Optional<Usuario> findByEmail(String email);
 }
