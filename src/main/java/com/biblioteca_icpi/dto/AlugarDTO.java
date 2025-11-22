cd123408fddd18d76ab7fa418c4cd09fd79c0b0d
kayquesann <kayque.ferreirasan@gmail.com>
2025-11-21 21:36:44 -0300

    criação de DTOS

---
+++ src/main/java/com/biblioteca_icpi/dto/AlugarDTO.java
@@ -0,0 +1,23 @@
+package com.biblioteca_icpi.dto;
+
+public class AlugarDTO {
+
+    private Long idLivro;
+    private Long idUsuario;
+
+    public Long getIdLivro() {
+        return idLivro;
+    }
+
+    public void setIdLivro(Long idLivro) {
+        this.idLivro = idLivro;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
---
+++ src/main/java/com/biblioteca_icpi/dto/EditarLivroDTO.java
@@ -0,0 +1,44 @@
+package com.biblioteca_icpi.dto;
+
+public class EditarLivroDTO {
+
+    private String nome;
+
+    private String autor;
+
+    private String descricao;
+
+    private String genero;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
